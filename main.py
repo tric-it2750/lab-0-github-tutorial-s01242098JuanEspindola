a5c7c05ff8261ecf845ec3c4b7504397057ff770
mdcrowley <matthew.crowley@tri-c.edu>
2025-01-13 19:49:37 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+######################################
+## WARNING! DO NOT MODIFY THIS FILE ##
+######################################
+
+def main():
+    return
+if __name__ == "__main__":
+    main()
